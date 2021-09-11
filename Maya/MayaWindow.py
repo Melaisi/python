93ecff5aa38ee9dbf35dbf9751e4587829c5146f
Mohammed Melaisi <mohammed.melaisi@gmail.com>
2021-09-10 21:31:33 -0400

    A sript to build basic maya window

---
+++ Maya/MayaWindow.py
@@ -0,0 +1,13 @@
+'''
+The goal of this script to display a non funcitonal user interface with elements 
+to create turntable with multiple options.
+
+This file hosted in external directory to be called by a python script hosted in Maya->Script folder 
+
+Resourcces:
+   Practical Maya Programming with Python - Galanakis, Robert, 
+    Chapter 5. Building Graphical User Interfaces for Maya
+
+
+'''
+
