8515457f04951d7cb44d9b6506de1721224bec87
Mohammed Melaisi <mohammed.melaisi@gmail.com>
2021-09-12 18:34:26 -0400

    Turntable Maya tool GUI script

---
+++ Maya/TurntableGUI.py
@@ -0,0 +1,15 @@
+'''
+:author:
+    Melaisi
+
+:synopsis:
+    This script contain the GUI elements of turntable tool for Maya
+
+:description:
+  Containt multiple options that will be used by turntable logic to create 
+  a turntable camera render in Maya
+
+
+'''
+import sys
+from PySide2 
