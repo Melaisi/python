847fc092e3012b1c61b4ac38e7d027a1b7e0deab
Mohammed Melaisi <mohammed.melaisi@gmail.com>
2021-09-12 21:53:19 -0400

    Getting Maya main window refrence and pass it as tool window parent

--- Maya/TurntableTool.py
+++ Maya/TurntableTool.py
@@ -1,3 +1,7 @@
+#For getting maya main window 
+from PySide2.QtWidgets import QMainWindow
+from shiboken2 import wrapInstance
+import maya._OpenMayaUI as OpenMayaUI
 
 import TurntableGUI 
 _window = None
@@ -5,5 +9,16 @@ def show():
     global _window
     if _window is None:
         controller = TurntableGUI.TurntableController()
-        _window = TurntableGUI.create_window(controller)
+        parent = get_maya_window()
+        _window = TurntableGUI.create_window(controller,parent)
     _window.show()
+
+def get_maya_window():
+    """Return the QMainWindow for the main Maya window """
+    """Source: practical maya programming book"""
+    winptr = OpenMayaUI.MQtUtil_mainWindow()
+    if winptr is None:
+        raise RuntimeError(winptr)
+    window = wrapInstance(long(winptr),QMainWindow)
+    assert isinstance(window,QMainWindow)
+    return window
