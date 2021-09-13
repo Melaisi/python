dfb9d846e0f879b0afdb68c51c0e0e91a2024df2
Mohammed Melaisi <mohammed.melaisi@gmail.com>
2021-09-12 23:31:55 -0400

    update slider and input values when changed, emit the values to listener

--- Maya/TurntableGUI.py
+++ Maya/TurntableGUI.py
@@ -32,15 +32,12 @@ class SliderType:
     INT = 1
     DOUBLE = 2
 
-class TurntableController(QtCore.QObject):
-    selectionChanged = QtCore.Signal(list)
-
 
 class TurntableWindow(QMainWindow):
-    createClicked = QtCore.Signal([])
+    createClicked = QtCore.Signal(list)
 
 
-def create_window(controller,parent=None):
+def create_window(parent=None):
     window = TurntableWindow(parent)
     window.setWindowTitle("Turntable Camera")
     main_layout = QVBoxLayout()
@@ -52,12 +49,16 @@ def create_window(controller,parent=None):
 
     # Buttons
     create_button = QPushButton("Create turntable")
+    def create_button_onClick():
+        args = [distance.value,height.value,degrees.value]
+        window.createClicked.emit(args)
+    create_button.clicked.connect(create_button_onClick)
 
 
     main_layout.addLayout(distance.layout)
     main_layout.addLayout(height.layout)
     main_layout.addLayout(degrees.layout)
-    
+
     main_layout.addWidget(create_button)
     
 
@@ -68,10 +69,17 @@ def create_window(controller,parent=None):
 
 
 class MySlider():
+    
+    def update_value(value):
+        '''Update both slider and input values to match '''
+        print(value)
+
     def __init__(self,label,type):
         self.layout = QHBoxLayout()
         self.label = QLabel(label)
         
+        self.value = 0
+
         self.input_line = QLineEdit()
         if(type == 1):
             self.validator = QtGui.QIntValidator()
@@ -81,9 +89,24 @@ class MySlider():
             self.validator = QtGui.QDoubleValidator()
 
         self.input_line.setValidator(self.validator)        
+        
         self.slider = QSlider(QtCore.Qt.Horizontal)
+
+        def update_slider(value):
+            self.slider.setValue(float(value))
+            self.value = float(value)
+        def update_input(value):
+            self.input_line.setText(str(value))
+            self.value = value
+        
+        self.input_line.textChanged.connect(update_slider)
+        self.slider.valueChanged.connect(update_input)
 
         self.layout.addWidget(self.label)
         self.layout.addWidget(self.input_line)
         self.layout.addWidget(self.slider)
+    
+    
+    
+
 
