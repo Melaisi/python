dabc01272f925c58d70d06b431bfd1e15626f3db
Mohammed Melaisi <mohammed.melaisi@gmail.com>
2021-09-12 22:28:00 -0400

    Basic layout of sliders and create button

--- Maya/TurntableGUI.py
+++ Maya/TurntableGUI.py
@@ -20,12 +20,17 @@ from PySide2.QtWidgets import (
     QDialog,
     QHBoxLayout,
     QVBoxLayout,
+    QLabel,
     QPushButton,
     QLineEdit,
     QSlider,
     QWidget,
     
 )
+
+class SliderType:
+    INT = 1
+    DOUBLE = 2
 
 class TurntableController(QtCore.QObject):
     selectionChanged = QtCore.Signal(list)
@@ -38,7 +43,47 @@ class TurntableWindow(QMainWindow):
 def create_window(controller,parent=None):
     window = TurntableWindow(parent)
     window.setWindowTitle("Turntable Camera")
+    main_layout = QVBoxLayout()
+
+    # Sliders
+    distance = MySlider("Distance", SliderType.DOUBLE)
+    height = MySlider("Height", SliderType.DOUBLE)
+    degrees = MySlider("Degrees Per Frame", SliderType.DOUBLE)
+
+    # Buttons
+    create_button = QPushButton("Create turntable")
+
+
+    main_layout.addLayout(distance.layout)
+    main_layout.addLayout(height.layout)
+    main_layout.addLayout(degrees.layout)
+    
+    main_layout.addWidget(create_button)
+    
+
     central_container = QWidget(window)
+    central_container.setLayout(main_layout)
     window.setCentralWidget(central_container)
     return window
+
+
+class MySlider():
+    def __init__(self,label,type):
+        self.layout = QHBoxLayout()
+        self.label = QLabel(label)
+        
+        self.input_line = QLineEdit()
+        if(type == 1):
+            self.validator = QtGui.QIntValidator()
+        elif(type == 2):
+             self.validator = QtGui.QDoubleValidator()
+        else:
+            self.validator = QtGui.QDoubleValidator()
+
+        self.input_line.setValidator(self.validator)        
+        self.slider = QSlider(QtCore.Qt.Horizontal)
+
+        self.layout.addWidget(self.label)
+        self.layout.addWidget(self.input_line)
+        self.layout.addWidget(self.slider)
 
